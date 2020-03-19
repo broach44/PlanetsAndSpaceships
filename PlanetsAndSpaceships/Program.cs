f4261c62c0e314485da7197274c05e54293e22b1
Crystal Broach <crysland@gmail.com>
2020-03-18 22:05:06 -0500

    Add project files.

---
+++ PlanetsAndSpaceships/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PlanetsAndSpaceships
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> planetList = new List<string>() { "Mercury", "Mars" };
+            List<string> lastTwoPlanets = new List<string>() { "Uranus", "Neptune" };
+
+            planetList.Add("Jupiter");
+            planetList.Add("Saturn");
+
+            planetList.AddRange(lastTwoPlanets);
+            planetList.Insert(1, "Venus");
+            planetList.Insert(2, "Earth");
+            planetList.Add("Pluto");
+            var rockyPlanets = planetList.GetRange(0, 4);
+            planetList.Remove("Pluto");
+            Console.WriteLine($"{String.Join(", ", planetList)}");
+            Console.WriteLine($"These are the rocky planets: {string.Join(", ", rockyPlanets)}");
+            Console.ReadLine();
+
+        }
+    }
+}
